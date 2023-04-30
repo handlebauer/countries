e38662ae6c7370c17900fa8c345aa92bf1026cdb
Donald Geddes <hi@donaldgeddes.ca>
2023-04-29 20:51:33 -0700

    Improve UK support; add normalizeCountry fn

---
+++ src/normalize-country.js
@@ -0,0 +1,25 @@
+import { eqCaseInsensitive, pipe, prop } from '@hbauer/convenience-functions'
+import { countries } from './data.js'
+
+/** @typedef {keyof countries[number] } CountryCodeType */
+
+/**
+ * @param {string} rawInput
+ * @param {{ inputType?: CountryCodeType, returnType?: CountryCodeType }} [opts]
+ */
+export const normalizeCountry = (
+  rawInput,
+  { inputType = 'name', returnType = 'alpha2' } = {}
+) => {
+  /**
+   * @param {countries[number]} input
+   */
+  const eqRawInput = input =>
+    pipe(input, prop(inputType), eqCaseInsensitive(rawInput))
+
+  if (rawInput === undefined || rawInput === null) return undefined
+
+  const country = countries.find(eqRawInput)
+  const code = country?.[returnType]
+  return code || null
+}
--- src/test.js
+++ src/test.js
@@ -1,5 +1,27 @@
 import test from 'ava'
+import { normalizeCountry } from './normalize-country.js'
 
-test('Should pass', t => {
-  t.pass()
+test('Should return undefined when null or undefined is passed', t => {
+  // @ts-expect-error
+  t.is(normalizeCountry(), undefined)
+  t.is(normalizeCountry(undefined), undefined)
+  t.is(normalizeCountry(null), undefined)
+})
+
+test('Should return null when an invalid input is passed', t => {
+  t.is(normalizeCountry('blahblahblah'), null)
+})
+
+test('Should return alpha2 code when valid input is passed', t => {
+  t.is(normalizeCountry('United States of America'), 'US')
+  t.is(normalizeCountry('England'), 'GB-ENG')
+  t.is(normalizeCountry('GB', { inputType: 'alpha2' }), 'GB')
+  t.is(
+    normalizeCountry('GBR', { inputType: 'alpha3', returnType: 'name' }),
+    'United Kingdom of Great Britain and Northern Ireland'
+  )
+  t.is(
+    normalizeCountry('GB-NIR', { inputType: 'alpha2', returnType: 'name' }),
+    'Northern Ireland'
+  )
 })
